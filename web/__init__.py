b8e14372cfa47fe637d55eb23740bfdf662cac79
Grace1615 <wxu40@uiowa.edu>
2022-04-04 14:25:34 +0800

    added styling

--- web/__init__.py
+++ web/__init__.py
@@ -5,19 +5,19 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'secretkey'
 
-connection = pymysql.connect(
-     host='csgo1m.cj3rigewkoph.us-east-2.rds.amazonaws.com',
-     user='admin',
-     password='CS4400GP9',
-     port=3306,
-     database='CSGOSTAT'
-)
-#test connection
-cursor = connection.cursor()
-query = "SELECT * from ban"
-cursor.execute(query)
-data = cursor.fetchone()
-print(data)
+# connection = pymysql.connect(
+#      host='csgo1m.cj3rigewkoph.us-east-2.rds.amazonaws.com',
+#      user='admin',
+#      password='CS4400GP9',
+#      port=3306,
+#      database='CSGOSTAT'
+# )
+# # test connection
+# cursor = connection.cursor()
+# query = "SELECT * from ban"
+# cursor.execute(query)
+# data = cursor.fetchone()
+# print(data)
 
 
 
