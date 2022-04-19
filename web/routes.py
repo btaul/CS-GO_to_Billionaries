629b77626621ac081833b603fbd75e20e325b23c
Brenden Taul <89364653+btaul@users.noreply.github.com>
2022-04-19 17:51:43 -0500

    halfway done with player stats

--- web/routes.py
+++ web/routes.py
@@ -1,8 +1,8 @@
 from web import app
-from flask import render_template,flash
+from flask import render_template, flash, request
 from web.forms import TeamForm
+from web.forms import SubmitForm
 from web import cursor
-
 
 
 @app.route('/')
@@ -10,9 +10,74 @@ def home():
     return render_template("home.html")
 
 
-@app.route('/player')
+@app.route('/index', methods=['GET'])
+def index():
+    return render_template("home.html")
+
+
+@app.route('/players', methods=['GET'])
 def player():
-    return render_template("player.html")
+    form = SubmitForm()
+    cursor.execute("SELECT * FROM events")
+    events = cursor.fetchall()
+    print("Total rows are:  ", len(events))
+    print("Printing each row")
+    e = []
+    for event in events:
+        print("Id: ", event[0])
+        print("Name: ", event[1])
+        e.append(event[1])
+        print("\n")
+
+    return render_template("player.html", events=e, form=form)
+
+
+@app.route('/players/event', methods=['POST'])
+def player_and_event():
+    form = SubmitForm()
+    event_name = request.form['event']
+    print("event_name: " + event_name)
+    query = "SELECT * FROM events WHERE event_name ='%s'" % event_name
+    cursor.execute(query)
+    event = cursor.fetchone()
+
+    print("Player + Event")
+    print("Id: ", event[0])
+    print("Name: ", event[1])
+
+    query2 = "SELECT match_id FROM matches WHERE event_id ='%s'" % event[0]
+    cursor.execute(query2)
+    matches = cursor.fetchmany()
+
+    m = []
+    for match in matches:
+        print("Match Id: ", match[0])
+        m.append(match[0])
+        print("\n")
+
+    return render_template("playerAndEvents.html", event=event[1], matches=m, form=form)
+
+
+@app.route('/players/event/match', methods=['POST'])
+def player_event_and_match():
+    form = SubmitForm()
+
+    match_id = request.form['match']
+    query2 = "SELECT * FROM matches WHERE match_id ='%s'" % match_id
+    cursor.execute(query2)
+    match = cursor.fetchone()
+
+    query = "SELECT * FROM events WHERE event_id ='%s'" % match[1]
+    cursor.execute(query)
+    event = cursor.fetchone()
+
+    print("Player + Event + Match")
+    print("Id: ", event[0])
+    print("Name: ", event[1])
+    print("Match Id: ", match[0])
+
+    return render_template("playerEventAndMatch.html", event=event[1], match=match[0], form=form)
+
 
 @app.route('/team', methods=['GET', 'POST'])
 def team():
